bfd1142bfce0ebc527eb2ab43a2542f7e9435f5b
sarahanne <sak@dgp.toronto.edu>
2020-02-13 18:59:30 -0500

    I mean it works (no)

--- include/igl/opengl/glfw/Viewer.cpp
+++ include/igl/opengl/glfw/Viewer.cpp
@@ -168,13 +168,13 @@ Eigen::Matrix4f getCurrentViewProjectionMatrix(vr::Hmd_Eye nEye)
 Eigen::Quaternionf GetRotation(vr::HmdMatrix34_t matrix) {
 	Eigen::Quaternionf q;
 
-	q.w = sqrt(fmax(0, 1 + matrix.m[0][0] + matrix.m[1][1] + matrix.m[2][2])) / 2;
-	q.x = sqrt(fmax(0, 1 + matrix.m[0][0] - matrix.m[1][1] - matrix.m[2][2])) / 2;
-	q.y = sqrt(fmax(0, 1 - matrix.m[0][0] + matrix.m[1][1] - matrix.m[2][2])) / 2;
-	q.z = sqrt(fmax(0, 1 - matrix.m[0][0] - matrix.m[1][1] + matrix.m[2][2])) / 2;
-	q.x = copysign(q.x, matrix.m[2][1] - matrix.m[1][2]);
-	q.y = copysign(q.y, matrix.m[0][2] - matrix.m[2][0]);
-	q.z = copysign(q.z, matrix.m[1][0] - matrix.m[0][1]);
+	q.w() = sqrt(fmax(0, 1 + matrix.m[0][0] + matrix.m[1][1] + matrix.m[2][2])) / 2;
+	q.x() = sqrt(fmax(0, 1 + matrix.m[0][0] - matrix.m[1][1] - matrix.m[2][2])) / 2;
+	q.y() = sqrt(fmax(0, 1 - matrix.m[0][0] + matrix.m[1][1] - matrix.m[2][2])) / 2;
+	q.z() = sqrt(fmax(0, 1 - matrix.m[0][0] - matrix.m[1][1] + matrix.m[2][2])) / 2;
+	q.x() = -1 * copysign(q.x(), matrix.m[2][1] - matrix.m[1][2]);
+	q.y() = -1 * copysign(q.y(), matrix.m[0][2] - matrix.m[2][0]);
+	q.z() = -1 *  copysign(q.z(), matrix.m[1][0] - matrix.m[0][1]);
 	return q;
 }
 

4974803e17e940ebf2111597f5f9254e7ceb3080
Cindy Zhu <cindyzhu0830@gmail.com>
2020-07-24 18:45:08 -0400

    minor

--- include/igl/opengl/ViewerCore.h
+++ include/igl/opengl/ViewerCore.h
@@ -38,8 +38,39 @@ namespace opengl
         struct Vector2 {
             float x;
             float y;
+
+            // ctors
             Vector2() : x(0), y(0) {};
             Vector2(float x, float y) : x(x), y(y) {};
+
+            // utils functions
+            void        set(float x, float y);
+            float       length() const;                         //
+            float       distance(const Vector2& vec) const;     // distance between two vectors
+            Vector2& normalize();                            //
+            float       dot(const Vector2& vec) const;          // dot product
+            bool        equal(const Vector2& vec, float e) const; // compare with epsilon
+
+            // operators
+            Vector2     operator-() const;                      // unary operator (negate)
+            Vector2     operator+(const Vector2& rhs) const;    // add rhs
+            Vector2     operator-(const Vector2& rhs) const;    // subtract rhs
+            Vector2& operator+=(const Vector2& rhs);         // add rhs and update this object
+            Vector2& operator-=(const Vector2& rhs);         // subtract rhs and update this object
+            Vector2     operator*(const float scale) const;     // scale
+            Vector2     operator*(const Vector2& rhs) const;    // multiply each element
+            Vector2& operator*=(const float scale);          // scale and update this object
+            Vector2& operator*=(const Vector2& rhs);         // multiply each element and update this object
+            Vector2     operator/(const float scale) const;     // inverse scale
+            Vector2& operator/=(const float scale);          // scale and update this object
+            bool        operator==(const Vector2& rhs) const;   // exact compare, no epsilon
+            bool        operator!=(const Vector2& rhs) const;   // exact compare, no epsilon
+            bool        operator<(const Vector2& rhs) const;    // comparison for sort
+            float       operator[](int index) const;            // subscript operator v[0], v[1]
+            float& operator[](int index);                  // subscript operator v[0], v[1]
+
+            friend Vector2 operator*(const float a, const Vector2 vec);
+            friend std::ostream& operator<<(std::ostream& os, const Vector2& vec);
         };
 
         struct VertexDataWindow
